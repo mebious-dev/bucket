9454baa4aa5bdf2ba35414534cdc3bab8d099fba
mebious <142015130+mebious-dev@users.noreply.github.com>
2023-10-03 19:00:48 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "mebious-dev/scoop-custom:main"
+    [String]$upstream = "mebious-dev/bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
